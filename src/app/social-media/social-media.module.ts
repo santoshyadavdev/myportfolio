ee99af4d3a55d7528a220560edb22b6bdc4f0dc7
Santosh Yadav <santosh.yadav198613@gmail.com>
2021-05-02 22:41:12 +0530

    remove unwanted code

--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SocialMediaComponent } from './social-media.component';
 import { BulmaBoxModule } from 'ngx2-bulma';
-import { BuyMeCofeeModule } from '../buy-me-cofee/buy-me-cofee.module';
 
 @NgModule({
   declarations: [SocialMediaComponent],
   imports: [
     CommonModule,
     BulmaBoxModule,
-    BuyMeCofeeModule
   ],
   exports: [SocialMediaComponent]
 })
