cce85d54055211e66d606ab9893571a029b355fa
Santosh Yadav <santosh.yadav198613@gmail.com>
2021-05-02 22:53:01 +0530

    upgrade to angular 11

--- src/app/containers/event/event.container.spec.ts
+++ src/app/containers/event/event.container.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EventContainer } from './event.container';
 
@@ -6,7 +6,7 @@ describe('EventContainer', () => {
   let component: EventContainer;
   let fixture: ComponentFixture<EventContainer>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EventContainer ]
     })
--- src/app/containers/recentblogs/recentblogs.component.spec.ts
+++ src/app/containers/recentblogs/recentblogs.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RecentblogsComponent } from './recentblogs.component';
 
@@ -6,7 +6,7 @@ describe('RecentblogsComponent', () => {
   let component: RecentblogsComponent;
   let fixture: ComponentFixture<RecentblogsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RecentblogsComponent ]
     })
