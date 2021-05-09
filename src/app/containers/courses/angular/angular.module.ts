a70b47d1e51b505b897992c6329772c8ab6ae64f
Santosh Yadav <santosh.yadav198613@gmail.com>
2021-05-10 00:35:19 +0530

    add component and modules for course

---
+++ src/app/containers/courses/angular/angular.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AngularRoutingModule } from './angular-routing.module';
+import { AngularComponent } from './angular.component';
+
+
+@NgModule({
+  declarations: [
+    AngularComponent
+  ],
+  imports: [
+    CommonModule,
+    AngularRoutingModule
+  ]
+})
+export class AngularCourseModule { }
