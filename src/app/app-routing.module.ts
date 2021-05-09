e56e33147b8a8f4976e1ca464511444807b1f5fe
Santosh Yadav <santosh.yadav198613@gmail.com>
2021-05-10 00:42:15 +0530

    change route name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   { path: 'events', loadChildren: () => import('./events/events.module').then(m => m.EventsModule) },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'show', loadChildren: () => import('./containers/show/show.conatiner.module').then(m => m.ShowContainerModule) },
-  { path: 'blogs', loadChildren: () => import('./containers/blogs-container/blogs-container.module').then(m => m.BlogsContainerModule) }
+  { path: 'blogs', loadChildren: () => import('./containers/blogs-container/blogs-container.module').then(m => m.BlogsContainerModule) },
+  { path: 'angular-course', loadChildren: () => import('./containers/courses/angular/angular.module').then(m => m.AngularCourseModule) }
 ];
 
 @NgModule({
